a596a49a268ecdecef5787ba876fab3f4c546d78
mayssa <60172570+mayssa-bm@users.noreply.github.com>
2021-07-26 13:58:04 +0100

    Update getDockerTag.groovy

--- vars/getDockerTag.groovy
+++ vars/getDockerTag.groovy
@@ -1,4 +1,4 @@
 def call(){
-    def tag = sh script: 'git init && git rev-parse HEAD', returnStdout: true
+    def tag = sh script: 'git init && git ls-remote https://github.com/mayssa-bm/k8s-CICD.git refs/heads/master', returnStdout: true
     return tag
 }
